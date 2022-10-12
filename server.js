d0b2115a9f0ec4f3960c0f119efe89d7a31db6d6
Niteesh <niteeshpatil7259@gmail.com>
2022-10-12 15:35:46 +0530

    First Commit task Manager app

---
+++ server.js
@@ -0,0 +1,31 @@
+const express=require('express')
+const cors=require('cors')
+const path=require('path')
+const assert=require('assert')
+const taskRoute=require('./route/taskRoute')
+
+const port=5000
+
+const app=express()
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.use(cors())
+
+//views engines
+app.set('view engine','ejs')
+app.set('views','./views')
+
+//default route
+app.use('/',taskRoute)
+
+//pnf route
+app.all(`*`,(req,res)=>{
+    res.render('pnf')
+})
+
+//server call
+app.listen(port,()=>{
+    console.log(`server is running @http://localhost:${port}`);
+})
